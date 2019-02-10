af32b34a9bdb466d7e36d096b31d1b0a6684ae53
ImprezaRSC <46632364+ImprezaRSC@users.noreply.github.com>
2019-02-09 21:09:32 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import subprocess
 
 class Killroscore(MycroftSkill):
     def __init__(self):
@@ -8,8 +8,9 @@ class Killroscore(MycroftSkill):
     @intent_file_handler('killroscore.intent')
     def handle_killroscore(self, message):
         self.speak_dialog('killroscore')
-
-
+        s="killall -e roscore"
+        subprocess.call([s],shell=True)
+        
 def create_skill():
     return Killroscore()
 
